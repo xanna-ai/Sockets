a25954fc3a7a82d32c3518ad44678998bb95325e
Anna <annaroman1990@mail.ru>
2022-09-18 22:22:11 +0300

    Sockets
    Sockets

---
+++ CLient 5C/CLient 5C/CLient 5C.c
@@ -0,0 +1,170 @@
+﻿#include <iostream>
+#include <sstream>
+#include <string>
+#include <time.h>
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <windows.h>
+
+// Для корректной работы нужна линковка с библиотекой Ws2_32.lib
+#pragma comment(lib, "Ws2_32.lib")
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+// Размер буферов
+const int MAXLEN = 1024;
+// IP адрес сервера
+char serverAddress[1024];
+// Порт сервера
+unsigned int serverPort;
+// Сообщение для сервера
+const char* clientMessage = "Манджиева Анна Романовна М30-324Б-19";
+
+// Функция отправки HTTP запроса и получения HTTP ответа
+// sockfd - дескриптор сокета 
+/*
+void SendHTTPRequest(int sockfd)
+{
+	//HTTP GET
+	//std::string get_http = "GET / HTTP/1.1\r\nHost: " + url + "\r\nConnection: close\r\n\r\n";
+	std::stringstream get_http;
+	get_http << "GET /index.html HTTP/1.1\r\nHost: "
+		<< url
+		<< "\r\nConnection: close\r\n\r\n";
+
+	// Отправляем запрос
+	send(sockfd, get_http.str().c_str(), strlen(get_http.str().c_str()), 0);
+
+	// Кол-во полученных байт
+	int nread;
+	// Буфер для получения ответа
+	char buf[MAXLEN];
+
+	while ((nread = recv(sockfd, buf, sizeof(buf), 0)) > 0)
+	{
+		printf("%s", buf);
+	}
+}*/
+
+int main()
+{
+	// Получаем адрес и порт сервера
+	FILE* settings;
+	if ((settings = fopen("serverinfo.txt", "r")) == NULL) {
+		printf("Cannot open settings file.\n");
+		exit(1);
+	}
+
+	fgets(serverAddress, 1024, settings);
+	serverAddress[strlen(serverAddress) - 1] = '\0';
+	char bufport[1024] = { 0 };
+	fgets(bufport, 1024, settings);
+	serverPort = atoi(bufport);
+	fclose(settings);
+
+	// Открытие файла логгирования
+	FILE* logFile;
+	if ((logFile = fopen("log.txt", "a")) == NULL) {
+		printf("Cannot open log file.\n");
+		exit(1);
+	}
+
+	// Служебная структура для хранения информации о реализации WindowsSocket
+	WSADATA wsadata;
+
+	int result = WSAStartup(MAKEWORD(2, 2), &wsadata);
+
+	// Если произошла ошибка
+	if (result != 0)
+	{
+		printf("WSAStartup failed: %s\n", result);
+		return result;
+	}
+
+	// Дескриптор сокета клиента
+	int sockfd;
+	struct sockaddr_in servaddr;
+
+	// Создаем сокет
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	// Если возникла ошибка при создании
+	if (sockfd == INVALID_SOCKET)
+	{
+		printf("INVALID SOCKET: %s\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	// Заполнение структуры адреса сокета
+	ZeroMemory(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(serverPort);
+	inet_pton(AF_INET, serverAddress, &servaddr.sin_addr);
+
+	// Соединение с сервером
+	result = connect(sockfd, (sockaddr*)&servaddr, sizeof(servaddr));
+
+	if (result != 0)
+	{
+		printf("Ошибка подключения к серверу: %s\n", serverAddress);
+		closesocket(sockfd);
+		return -1;
+	}
+
+	// задержка
+	Sleep(2000);
+
+	time_t tt;
+	struct tm* ptm;
+	char buf[BUFSIZ];
+
+	// Время подключения
+	tt = time(NULL);
+	ptm = localtime(&tt);
+	strftime(buf, BUFSIZ, "%d-%m-%Y", ptm);
+
+	fprintf(logFile, "\n%s", asctime(ptm));
+
+	// Адрес сервера(лог)
+	fprintf(logFile, "Cоединение с сервером: %s\n", serverAddress);
+
+
+	// Отправляем сообщение серверу
+	send(sockfd, clientMessage, strlen(clientMessage), 0);
+
+	// Время отправки сообщения
+	tt = time(NULL);
+	ptm = localtime(&tt);
+	strftime(buf, BUFSIZ, "%d-%m-%Y", ptm);
+
+	fprintf(logFile, "\n%s", asctime(ptm));
+
+	// Отправленное сообщение(лог)
+	fprintf(logFile, "Введите сообщение: %s\n", clientMessage);
+
+	// Кол-во полученных байт
+	int nread;
+	// Буфер для получения ответа
+	char bufAnswer[MAXLEN];
+
+	// Ждем ответ от сервера
+	nread = recv(sockfd, bufAnswer, sizeof(bufAnswer), 0);
+	bufAnswer[nread] = '\0';
+
+	// Время получения ответа
+	tt = time(NULL);
+	ptm = localtime(&tt);
+	strftime(buf, BUFSIZ, "%d-%m-%Y", ptm);
+
+	fprintf(logFile, "\n%s", asctime(ptm));
+
+	fprintf(logFile, "Ответ от сервера: %s\n", bufAnswer);
+
+	fclose(logFile);
+	closesocket(sockfd);
+	WSACleanup();
+}
+
---
+++ Server/LB5 IT/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+//using System.Threading.Tasks;
+
+namespace SocketTcpServer
+{
+    class Program
+    {
+        static void write(string fullpath, string message)
+        {
+            using (StreamWriter sw = new StreamWriter(fullpath, true))
+            {
+                sw.WriteLine(DateTime.Now.ToString() + "    INFO    " + message);
+            }
+        }
+        static int port = 23000; // порт для приема входящих запросов
+        static string path = "serverlog.txt";
+        static void Main(string[] args)
+        {
+            // получаем адреса для запуска сокета
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("172.20.10.3"), port);
+
+            // создаем сокет
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            write(path, "Socket initialisation succesful!");
+            write(path, "ServerIP:" + ipPoint);
+            write(path, "Port:" + port);
+            try
+            {
+                // связываем сокет с локальной точкой, по которой будем принимать данные
+                listenSocket.Bind(ipPoint);
+                write(path, "Socket Local Point connection succesful!");
+                Console.WriteLine("Socket Local Point connection succesful!");
+                // начинаем прослушивание
+                write(path, "Server launched! Start listening...");
+                listenSocket.Listen(10);
+
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    write(path, "New query!");
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байтов
+                    byte[] data = new byte[256]; // буфер для получаемых данных
+
+                    do
+                    {
+                        write(path, "Receiving data...");
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+                    write(path, "Received query data: " + builder.ToString());
+                    Console.WriteLine("Received query data: " + builder.ToString());
+                    Console.WriteLine("Processing data");
+                    write(path, "Processing data");
+                    char letter;
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+                    for (int i = 0; i < builder.Length / 2; i++)
+                    {
+                        if (i == (builder.Length - 1 - i))
+                            break;
+                        letter = builder[i];
+                        builder[i] = builder[builder.Length - 1 - i];
+                        builder[builder.Length - 1 - i] = letter;
+                    }
+                    builder.Append(" автор сервера: Манджиева Анна Романовна М30-324Б-19.");
+                    Console.WriteLine("Processed data: " + builder.ToString());
+                    Console.WriteLine("sending data...");
+                    // отправляем ответ
+                    string message = "ваше сообщение доставлено";
+                    data = Encoding.UTF8.GetBytes(message);
+                    handler.Send(data);
+                    // закрываем сокет
+                    write(path, "Datas been sent succesfully!");
+                    Console.WriteLine("Datas been sent succesfully!");
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
